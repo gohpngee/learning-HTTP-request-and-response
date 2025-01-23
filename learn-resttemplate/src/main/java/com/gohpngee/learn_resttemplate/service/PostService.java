8647c84c98004da6f86c2c98d3ae895ab544d8d2
Brian Goh <gohpngee@gmail.com>
2025-01-24 03:07:40 +0800

    trying to contruct the method for getAllPosts in the controller layer, as well as a fetchAllPosts method in the service layer

--- learn-resttemplate/src/main/java/com/gohpngee/learn_resttemplate/service/PostService.java
+++ learn-resttemplate/src/main/java/com/gohpngee/learn_resttemplate/service/PostService.java
@@ -1,4 +1,20 @@
 package com.gohpngee.learn_resttemplate.service;
 
+import com.gohpngee.learn_resttemplate.model.Post;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
 public class PostService {
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public List<Post> fetchAllPosts() {
+        String url = "https://jsonplaceholder.typicode.com/posts";
+
+        //uses the RestTemplate GET method to get the HTTP response and map it into an array of Post objects.
+        Post[] posts = restTemplate.getForObject(url, Post[].class);
+
+    }
 }
