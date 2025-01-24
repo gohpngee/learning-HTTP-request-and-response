e67cf468aa28b6d74e1ad3344eb6a64dbfcfe5e7
Amelia Lee <amelialee@Amelias-MacBook-Air.local>
2025-01-24 17:04:08 +0800

    added mapping for home, as well as methods for getAllPosts and getIdPost using ResponseEntity

--- learn-resttemplate/src/main/java/com/gohpngee/learn_resttemplate/controller/PostController.java
+++ learn-resttemplate/src/main/java/com/gohpngee/learn_resttemplate/controller/PostController.java
@@ -5,6 +5,7 @@ import com.gohpngee.learn_resttemplate.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,10 +22,29 @@ public class PostController {
         this.postService = postService;
     }
 
-    @GetMapping("/posts")
-    public ResponseEntity<List<Post>> getAllPosts() {
-        String url = "https://jsonplaceholder.typicode.com/posts";
+    @GetMapping("/home")
+    public String home() {
+        return "Welcome to the Post API!";
+    }
 
-        Post[] posts =
+    @GetMapping("/posts")
+    //ResponseEntity returns data for successful requests, and a HTTP Status code for errors
+    public ResponseEntity<List<Post>> getAllPosts() {
+        List<Post> posts = postService.fetchAllPosts();
+        if (posts.isEmpty()) {
+            //usage of .build() to set the response to 204 or 404
+            //and avoid returning null value
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(posts); //returns a 200 OK with JSON arrayList of posts
+    }
+
+    @GetMapping(value = "/posts/{id}", produces = "application/json")
+    public ResponseEntity<Post> getIdPost(@PathVariable int id) {
+        Post post = postService.fetchIdPost(id);
+        if (post == null) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(post); // HTTP response 200, OK
     }
 }
