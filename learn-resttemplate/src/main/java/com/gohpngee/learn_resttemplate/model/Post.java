a233dff843aa5b9970b752d806aa2376dbda44da
Amelia Lee <amelialee@Amelias-MacBook-Air.local>
2025-02-03 18:30:19 +0800

    made changes

--- learn-resttemplate/src/main/java/com/gohpngee/learn_resttemplate/model/Post.java
+++ learn-resttemplate/src/main/java/com/gohpngee/learn_resttemplate/model/Post.java
@@ -1,5 +1,6 @@
 package com.gohpngee.learn_resttemplate.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -7,14 +8,19 @@ import lombok.Setter;
 @Getter
 @Setter
 @RequiredArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Post {
     private int userId;
     private int id;
     private String title;
     private String body;
-    //private int titleLength;
 
-    /*public int getTitleLength() {
-        return title.length();
-    }*/
+    @Override
+    public String toString() {
+        return "Post{" +
+                "userId = " + userId +
+                "id = " + id +
+                "title = " + title +
+                "body = " + body + "}";
+    }
 }
