60209017263b8dadaadfd650f79e41f424d41464
Brian Goh <gohpngee@gmail.com>
2025-02-10 02:56:21 +0800

    removed RestTemplate import as unused

--- learn-resttemplate/src/main/java/com/gohpngee/learn_resttemplate/LearnResttemplateApplication.java
+++ learn-resttemplate/src/main/java/com/gohpngee/learn_resttemplate/LearnResttemplateApplication.java
@@ -2,7 +2,6 @@ package com.gohpngee.learn_resttemplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class LearnResttemplateApplication {
