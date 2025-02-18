90e22cd43fed79e6fe81f285c04e26764807f0c4
Brian Goh <gohpngee@gmail.com>
2025-02-18 14:06:35 +0800

    added explicitly declared getters and setters

--- learn-resttemplate/src/main/java/com/gohpngee/learn_resttemplate/model/Post.java
+++ learn-resttemplate/src/main/java/com/gohpngee/learn_resttemplate/model/Post.java
@@ -1,27 +1,45 @@
 package com.gohpngee.learn_resttemplate.model;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @Data
-@RequiredArgsConstructor
 @NoArgsConstructor
-@ToString
-
+@AllArgsConstructor
 public class Post {
     private int userId;
     private int id;
     private String title;
     private String body;
 
-    @Override
-    public String toString() {
-        return "Post{" +
-                "userId = " + userId +
-                "id = " + id +
-                "title = " + title +
-                "body = " + body + "}";
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
     }
 }
