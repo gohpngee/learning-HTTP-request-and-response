fd792fa9c9d069a0a963760cafefc31876a74c3a
Brian Goh <gohpngee@gmail.com>
2025-02-19 03:53:01 +0800

    added JsonProperty, as well as getters and setters, and new field for titleLength, and getter for it

--- learn-resttemplate/src/main/java/com/gohpngee/learn_resttemplate/model/Post.java
+++ learn-resttemplate/src/main/java/com/gohpngee/learn_resttemplate/model/Post.java
@@ -1,45 +1,30 @@
 package com.gohpngee.learn_resttemplate.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Post {
+    @JsonProperty("userId")
     private int userId;
+    @JsonProperty("id")
     private int id;
+    @JsonProperty("title")
     private String title;
+    @JsonProperty("body")
     private String body;
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
+    @JsonIgnore
+    private int titleLength;
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
+    public int getTitleLength() {
+        return title != null ? title.length() : 0;
     }
 }
