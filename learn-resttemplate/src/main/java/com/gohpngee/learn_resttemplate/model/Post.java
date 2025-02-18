8e163507798ec5549e53bc9e5620a60975850bb7
Amelia Lee <amelialee@Amelias-MacBook-Air.local>
2025-02-18 21:27:53 +0800

    changed lombok annotation to just getters and setters, and also explicitly declaring JsonProperty for variables

--- learn-resttemplate/src/main/java/com/gohpngee/learn_resttemplate/model/Post.java
+++ learn-resttemplate/src/main/java/com/gohpngee/learn_resttemplate/model/Post.java
@@ -1,45 +1,19 @@
 package com.gohpngee.learn_resttemplate.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Post {
+    @JsonProperty("userId")
     private int userId;
+    @JsonProperty("idd")
     private int id;
+    @JsonProperty("title")
     private String title;
+    @JsonProperty("body")
     private String body;
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
 }
