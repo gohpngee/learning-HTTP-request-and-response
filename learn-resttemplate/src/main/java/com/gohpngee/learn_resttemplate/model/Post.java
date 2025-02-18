7d297bb736ac2f214397651b4b1e8757c09abe30
Brian Goh <gohpngee@gmail.com>
2025-02-19 03:33:40 +0800

    added titleLength

--- learn-resttemplate/src/main/java/com/gohpngee/learn_resttemplate/model/Post.java
+++ learn-resttemplate/src/main/java/com/gohpngee/learn_resttemplate/model/Post.java
@@ -1,19 +1,45 @@
 package com.gohpngee.learn_resttemplate.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Post {
-    @JsonProperty("userId")
     private int userId;
-    @JsonProperty("idd")
     private int id;
-    @JsonProperty("title")
     private String title;
-    @JsonProperty("body")
     private String body;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
